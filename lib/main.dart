3d40a4314eae2d5ccb8ba79eb7cd5b1f6a6ad25e
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2026-02-25 16:23:19 +0200

    Initializing the project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
